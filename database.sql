fee8cdf0debe17d5a86896d72252df3e3cc0e3dd
Awab-Ahmed-Os <Awabahmed987@gmail.com>
2025-02-01 06:49:53 +0300

    Add files via upload

--- database.sql
+++ database.sql
@@ -1,56 +1,92 @@
--- MySQL dump 10.13  Distrib 8.0.41, for Win64 (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: global_hotels_booking
--- ------------------------------------------------------
--- Server version	8.0.41
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 01, 2025 at 03:47 AM
+-- Server version: 9.1.0
+-- PHP Version: 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `hotels table`
+--
+
+DROP TABLE IF EXISTS `hotels table`;
+CREATE TABLE IF NOT EXISTS `hotels table` (
+  `HotelID` int NOT NULL,
+  `Hotel_name` varchar(50) NOT NULL,
+  `Country` varchar(50) NOT NULL,
+  `City` varchar(50) NOT NULL,
+  `Address` text NOT NULL,
+  `Star_rate` int NOT NULL,
+  `Description` text NOT NULL,
+  `Amenities` json NOT NULL,
+  `ImageURLs` text NOT NULL,
+  PRIMARY KEY (`HotelID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `reports`
 --
 
 DROP TABLE IF EXISTS `reports`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `reports` (
+CREATE TABLE IF NOT EXISTS `reports` (
   `ReportID` int NOT NULL AUTO_INCREMENT,
   `UserID` int DEFAULT NULL,
   `ReportType` enum('Bookings','Revenues','Users','Hotels','Feedbacks') DEFAULT NULL,
   `ReportData` blob,
   `ReportDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`ReportID`),
-  KEY `UserID` (`UserID`),
-  CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`)
+  KEY `UserID` (`UserID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `reports`
+-- Table structure for table `rooms table`
 --
 
-LOCK TABLES `reports` WRITE;
-/*!40000 ALTER TABLE `reports` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reports` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP TABLE IF EXISTS `rooms table`;
+CREATE TABLE IF NOT EXISTS `rooms table` (
+  `RoomID` int NOT NULL,
+  `HotelID` int NOT NULL,
+  `Room_type` enum('Single','Double','Suite') NOT NULL,
+  `Occupancy_adults` int NOT NULL,
+  `Occupancy_children` int NOT NULL,
+  `Price_per_night` decimal(10,2) NOT NULL,
+  `Availability` tinyint(1) NOT NULL,
+  `Amenities` json NOT NULL,
+  `Bed_type` enum('King','Queen','Twin') NOT NULL,
+  PRIMARY KEY (`RoomID`),
+  UNIQUE KEY `Foreign Key` (`HotelID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
+CREATE TABLE IF NOT EXISTS `users` (
   `UserID` int NOT NULL AUTO_INCREMENT,
   `Email` varchar(50) NOT NULL,
   `Password` varchar(50) NOT NULL,
@@ -64,24 +100,24 @@ CREATE TABLE `users` (
   PRIMARY KEY (`UserID`),
   UNIQUE KEY `Email` (`Email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Constraints for dumped tables
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`UserID`) REFERENCES `users` (`UserID`);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Constraints for table `rooms table`
+--
+ALTER TABLE `rooms table`
+  ADD CONSTRAINT `rooms table_ibfk_1` FOREIGN KEY (`HotelID`) REFERENCES `hotels table` (`HotelID`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-01-31 13:59:54
